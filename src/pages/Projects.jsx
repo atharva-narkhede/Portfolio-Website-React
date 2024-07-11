646a0e7cf3ee2cf3b82f11a0ed5a7696d29a58d0
Atharva Narkhede <atharvan12345@gmail.com>
2024-07-11 18:18:29 +0530

    added images to projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,14 +2,6 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "../components/Projects/ProjectCard";
 import Particle from "../components/Particle";
-import photo_contest from "../assets/projects/photo-contest.png";
-import ecomm from "../assets/projects/ecomm.png";
-import weather from "../assets/projects/weather.png";
-import marketBasket from "../assets/projects/project.jpeg";
-import personalPortfolio from "../assets/projects/portfolioo.png";
-import alzheimerResearch from "../assets/projects/pg.png";
-import bmiCalculator from "../assets/projects/bmicalc.png";
-import stickynotes from "../assets/projects/stickynotes.png";
 
 const Projects = () => {
   return (
@@ -25,7 +17,7 @@ const Projects = () => {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={photo_contest}
+              imgPath="https://i.ibb.co/JpjBwWb/photo-contest.png"
               isBlog={false}
               title="Photography Contest Website"
               description="The Photography Contest Website, developed using the MERN stack, facilitates seamless participation for photographers in contests through user-friendly interfaces for photo uploads, voting, and contest management. Admins efficiently oversee contests with features for creation, updating, and deletion, ensuring smooth photo management. The platform fosters community engagement and talent showcasing, enhancing user experience and fostering a vibrant photography community."
@@ -37,7 +29,7 @@ const Projects = () => {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={ecomm}
+              imgPath="https://i.ibb.co/SJ2pQBR/ecomm.png"
               isBlog={false}
               title="E-Commerce MERN Stack"
               description="This project is a full-featured E-Commerce application built with ReactJS. It supports user authentication, product listings, and cart functionalities. The app is deployed on Firebase, and the backend API is hosted on Render."
@@ -49,7 +41,7 @@ const Projects = () => {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={weather}
+              imgPath="https://i.ibb.co/tx2zND2/weather.png"
               isBlog={false}
               title="Weather Forecast GUI with Python"
               description="The Weather Forecast App is a Python-based GUI application designed to provide comprehensive weather details for a specified location. It leverages various weather parameters to offer users a detailed and accurate forecast."
@@ -60,7 +52,7 @@ const Projects = () => {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={marketBasket}
+              imgPath="https://i.ibb.co/NZnJ41x/project.jpg"
               isBlog={false}
               title="Market Basket Analysis for Amazon Product Recommendation"
               description="Conducted Market Basket Analysis (MBA) on Amazon product dataset to enhance recommendations. Identified top-selling products and top products in each category using review count. Implemented association rule mining for personalized recommendations. Evaluated effectiveness through metrics."
@@ -71,7 +63,7 @@ const Projects = () => {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={personalPortfolio}
+              imgPath="https://i.ibb.co/qBZNHWZ/portfolioo.png"
               isBlog={false}
               title="Personal Portfolio App with Flutter"
               description="Developed a personal portfolio app using Flutter, featuring various pages including skills, projects, education, contact, and certificates. Implemented a clean and user-friendly interface to showcase skills and projects effectively. Utilized Flutter’s widgets and plugins to create dynamic and interactive pages, enhancing user experience and engagement."
@@ -82,7 +74,7 @@ const Projects = () => {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={alzheimerResearch}
+              imgPath="https://i.ibb.co/2S2bLXT/pg.png"
               isBlog={false}
               title="Alzheimer Disease Detection Research Project"
               description="Conducted research on Alzheimer’s disease detection using a dataset and employing fuzzy rough set theory along with machine learning algorithms such as Support Vector Machine (SVM) and k-Nearest Neighbors (KNN). Achieved 90% accuracy in disease prediction using SVM."
@@ -93,7 +85,7 @@ const Projects = () => {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bmiCalculator}
+              imgPath="https://i.ibb.co/k55Wqr6/bmicalc.png"
               isBlog={false}
               title="BMI Calculator App using Flutter"
               description="The BMI Calculator Flutter App, built with Flutter and Dart, swiftly calculates Body Mass Index (BMI) for both Android and iOS users. It seamlessly guides users through entering their age, height, weight, and gender, then provides personalized insights and ideal weight ranges based on their calculated BMI. This user-friendly app features a clean, intuitive interface and clear instructions, making it accessible to people of all technical backgrounds."
@@ -101,9 +93,10 @@ const Projects = () => {
               ghLink="https://github.com/atharva-narkhede/bmi_calculator_flutter"
             />
           </Col>
+          
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={stickynotes}
+              imgPath="https://i.ibb.co/r0DX0jr/stickynotes.png"
               isBlog={false}
               title="Sticky Notes App"
               description="Sticky Notes App is a simple and intuitive Android application developed in Android Studio using Java. It allows users to create, edit, and manage their notes conveniently. This app is designed to be user-friendly, providing a seamless experience for organizing your thoughts and tasks on the go."
